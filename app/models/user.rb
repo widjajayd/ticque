211112a408f8025c92a132bf369b49759b5e43a3
widjajayd <widjajaydror@gmail.com>
2022-08-23 15:09:59 +0700

    install bootstrap finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
+
+
